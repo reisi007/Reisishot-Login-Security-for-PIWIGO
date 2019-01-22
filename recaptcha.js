f0b8dc4aaeac6001aa8ff74cb3e90ccd520d9801
Florian Reisinger <reisi007@gmail.com>
2019-01-22 18:13:38 +0100

    Update recaptcha.js

--- recaptcha.js
+++ recaptcha.js
@@ -1,15 +1,17 @@
 // Minified using https://jscompress.com/
 (function () {
-    var form = document.getElementById('password');
-    if (!form)
-        return;//No action needed
-    var silb = form.parentNode.nextSibling;
-    var form = silb.parentNode;
-    var div = document.createElement("div");
-    form.insertBefore(div, silb);
-    setTimeout(function () {
-        grecaptcha.render(div, {
-            'sitekey': pk
-        });
+	window.onload = function(){
+		var form = document.getElementById('password');
+		if (!form)
+			return;//No action needed
+		var silb = form.parentNode.nextSibling;
+		var form = silb.parentNode;
+		var div = document.createElement("div");
+		form.insertBefore(div, silb);
+		setTimeout(function () {
+			grecaptcha.render(div, {
+				'sitekey': pk
+			});
     }, 200);
+	};
 })();
